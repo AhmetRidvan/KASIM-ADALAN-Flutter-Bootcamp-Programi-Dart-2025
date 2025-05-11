e4126dbbf0c6128de5f8decb320669b05f105592
AhmetRidvan <ahmetridvanordulu@gmail.com>
2025-05-11 20:34:50 +0300

    sd
    sd

--- flag_quiz_app/lib/views/quiz_page.dart
+++ flag_quiz_app/lib/views/quiz_page.dart
@@ -13,11 +13,11 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<FlagModel> questions = [];
-  List<FlagModel> wrongOptions = [];
-  late FlagModel trueQuestion;
-  final allOptions = HashSet<FlagModel>();
-  int questionCount = 0;
+  List<FlagModel> flags = [];
+  List<FlagModel> falseFlags = [];
+  late FlagModel trueFlag;
+  final allFlagsForButtons = HashSet<FlagModel>();
+  int count = 0;
   int trueCount = 0;
   int wrongCount = 0;
 
@@ -34,12 +34,55 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   Future<void> bringTheQuestions() async {
-    questions = await FlagsDao.bringTheFlags5();
-    
+    flags = await FlagsDao.bringTheFlags5();
+    for (final x in flags) {
+      print('questions ${x.flag_id} ${x.flag_image} ${x.flag_name}');
+    }
     loadTheQuestions();
   }
 
-  Future<void> loadTheQuestions() async {}
+  Future<void> loadTheQuestions() async {
+    trueFlag = flags[count];
+
+    flagImageName = "${trueFlag.flag_name.toLowerCase()}.png";
+   
+    falseFlags = await FlagsDao.bringTheWrongFlags3(trueFlag.flag_id);
+
+    allFlagsForButtons.clear();
+    allFlagsForButtons.add(trueFlag);
+    allFlagsForButtons.add(falseFlags[0]);
+    allFlagsForButtons.add(falseFlags[1]);
+    allFlagsForButtons.add(falseFlags[2]);
+
+    buttonAText = allFlagsForButtons.elementAt(0).flag_name;
+    buttonBText = allFlagsForButtons.elementAt(1).flag_name;
+    buttonCText = allFlagsForButtons.elementAt(2).flag_name;
+    buttonDText = allFlagsForButtons.elementAt(3).flag_name;
+    setState(() {});
+  }
+
+  void countControl() {
+    count++;
+    if (count != 5) {
+      loadTheQuestions();
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) {
+            return ResultPage(numberOfCorrect: trueCount);
+          },
+        ),
+      );
+    }
+  }
+
+  void trueControl(String buttonText) {
+    if (trueFlag.flag_name == buttonText) {
+      trueCount++;
+    } else {
+      wrongCount++;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,42 +95,57 @@ class _QuizPageState extends State<QuizPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("True : 1", style: TextStyle(fontSize: 18)),
-                Text("False : 1", style: TextStyle(fontSize: 18)),
+                Text("True : $trueCount", style: TextStyle(fontSize: 18)),
+                Text("False : $wrongCount", style: TextStyle(fontSize: 18)),
               ],
             ),
-            Text("1.Question", style: TextStyle(fontSize: 30)),
-            Image.asset('assets/turkiye.png'),
+            count != 5
+                ? Text("${count + 1}.Question", style: TextStyle(fontSize: 30))
+                : Text("5.Question", style: TextStyle(fontSize: 30)),
+            Image.asset('assets/${flagImageName}'),
             SizedBox(
               width: 300,
               height: 70,
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return ResultPage(numberOfCorrect: 3);
-                      },
-                    ),
-                  );
+                  trueControl(buttonAText);
+                  countControl();
                 },
-                child: Text('Button A'),
+                child: Text(buttonAText),
               ),
             ),
             SizedBox(
               width: 300,
               height: 70,
-              child: ElevatedButton(onPressed: () {}, child: Text('Button B')),
+              child: ElevatedButton(
+                onPressed: () {
+                  trueControl(buttonBText);
+                  countControl();
+                },
+                child: Text(buttonBText),
+              ),
             ),
             SizedBox(
               width: 300,
               height: 70,
-              child: ElevatedButton(onPressed: () {}, child: Text('Button C')),
+              child: ElevatedButton(
+                onPressed: () {
+                  trueControl(buttonCText);
+                  countControl();
+                },
+                child: Text(buttonCText),
+              ),
             ),
             SizedBox(
               width: 300,
               height: 70,
-              child: ElevatedButton(onPressed: () {}, child: Text('Button D')),
+              child: ElevatedButton(
+                onPressed: () {
+                  trueControl(buttonDText);
+                  countControl();
+                },
+                child: Text(buttonDText),
+              ),
             ),
           ],
         ),
